b540dd1a8d7401b28bfd4f293ebe9c84b11c4082
Dhanalaxmi1314 <laxmidhanam2004@gmail.com>
2025-09-03 21:16:46 +0530

    Add files via upload

---
+++ C2TC_SpringProject/springbootIOC.DI/src/test/java/com/tnsif/IOC/DI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.IOC.DI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
